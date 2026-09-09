1d7ca162d93d2e7b0605a2154f67ec5a035d8c25
Puria Nafisi Azizi <puria@dyne.org>
2026-09-09 10:55:23 +0200

    build(docs): bundle fonts locally (#10)
    Avoid runtime requests to Google Fonts and keep documentation typography self-hosted.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
+import '@fontsource/archivo-black/400.css'
+import '@fontsource/fira-code/400.css'
+import '@fontsource/fira-code/500.css'
+import '@fontsource/fira-code/600.css'
 import './custom.css'
 import Layout from './Layout.vue'
 import './versions.css'
